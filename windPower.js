b0502e4c8228b7ff4cce26d3854b35a050b47fd4
Callum Pearce <cp15571@my.bristol.ac.uk>
2017-02-27 16:48:16 +0000

    circles added for map

--- windPower.js
+++ windPower.js
@@ -1,4 +1,20 @@
 "use strict"
-function initWindPower(){
-    alert("hello");
+function initWindPower(map, windData){
+    
+    for (var i = 0; i < windData.length; i++) { 
+        if(parseFloat(windData[i][2]) > 5.8){
+            var positionM = {lat: parseInt(windData[i][0]), lng: parseInt(windData[i][1])};
+            var marker = new google.maps.Marker({
+                position: positionM,
+                map: map,
+                icon: {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    scale: 10,
+                    fillColor: 'green',
+                    fillOpacity: 0.6,
+                    strokeWeight: 1,
+                }
+            });
+        }
+    }
 }
