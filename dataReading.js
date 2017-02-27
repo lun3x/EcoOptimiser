62bea2ca97f8be6262744b3ac318c7f05bb53f3a
Callum Pearce <cp15571@my.bristol.ac.uk>
2017-02-27 15:19:32 +0000

    csv parser working correctly

--- dataReading.js
+++ dataReading.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $.ajax({
         type: "GET",
         url: "http://localhost:8000/WindSpeed.csv",
         dataType: "text",
         success: function(data) {processData(data);}
      });
-});
+});*/
 
 function processData(data) {
     var result = $.csv.toArrays(data);
-    console.log(result);
+    return(result);
 }
